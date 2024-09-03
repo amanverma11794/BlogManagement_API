67ec7c45badfc2cc4e3b89029d53d257646ff738
amanverma11794 <amanverma11794@gmail.com>
2024-09-03 13:38:45 +0530

    New Build Updated

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Management_API.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Welcome to the Home Page!");
+        }
+    }
+
+}
